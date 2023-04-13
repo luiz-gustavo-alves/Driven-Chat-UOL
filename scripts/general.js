9b7b27d5681aed9e3aebc7d912f82bcaef5f6d44
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-04-13 18:07:00 -0300

    Created sendMessage script + userAuth features
    Function detail:
    sendMessage(): given user message input, send text message to everyone in the server.
    
    Features:
    userAuth(): check render messages and user status promise inside script.

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,8 @@
 let messages = [];
+let userName;
 
 const userURL = 'https://mock-api.driven.com.br/api/vm/uol/participants';
+const userStatusURL = 'https://mock-api.driven.com.br/api/vm/uol/status';
 const msgURL = 'https://mock-api.driven.com.br/api/vm/uol/messages';
 const token = 'uno6r9oP7lrt17ZaOROMIr8i';
 
@@ -33,7 +35,7 @@ function renderMessages() {
                     <span class="timestamp">(${message.time})</span> 
                     <span class="username">${message.from}</span>
                     <span class="text">para</span>
-                    <span class="everyone">Todos:</span>
+                    <span class="everyone">${message.to}:</span>
                     <span class="text">${message.text}</span>
                 </li>
             `;
@@ -43,39 +45,78 @@ function renderMessages() {
 
 function userAuth() {
 
-    const username = prompt("Qual é o seu nome?");
+    userName = prompt("Qual é o seu nome?");
 
     const authUser = {
-        name: username
+        name: userName
     };
 
-    const promise = axios.post(userURL, authUser);
-    promise.then(res => {
-            
-            console.log("usuario-logado");
-            console.log(res);
-        }
-    )
-    promise.catch(err => {
-            console.log("usuario-ja-logado");
-            userAuth();
-        }
-    )
+    const userAuthPromise = axios.post(userURL, authUser);
+
+    /* User authenticated */
+    userAuthPromise.then(() => {   
+
+        console.log("User authenticated.");
+
+        /* Check and render chat messages */
+        setInterval(() => {
+
+            const renderMessagesPromise = axios.get(msgURL);
+
+            renderMessagesPromise.then(res => {
+                console.log(res.data);
+                messages = res.data;
+                renderMessages();
+            });
+
+            renderMessagesPromise.catch(err => {
+                console.log(err);
+            });
+
+        }, 3000);
+
+        /* Check if user is still online */
+        setInterval(() => {
+
+            const userStatus = {
+                name: userName
+            };
+
+            axios.post(userStatusURL, userStatus);
+
+        }, 5000);
+    });
+
+    /* User not authenticated*/
+    userAuthPromise.catch(() => {
+
+        console.log("User not authenticated.");
+        userAuth();
+    });
+}
+
+function sendMessage() {
+
+    const messageInput = document.querySelector(".message-input");
+
+    const message = {
+        from: userName,
+        to: "Todos",
+        text: messageInput.value,
+        type: "message"
+    };
+
+    messageInput.value = '';
+
+    const sendMessagePromise = axios.post(msgURL, message);
+    sendMessagePromise.then(() => {
+        console.log("Message sent!");
+        
+    });
+    sendMessagePromise.catch(() => {
+        console.log("Message not sent - User offline!");
+        window.location.reload();
+    });
 }
 
 userAuth();
-
-setInterval(() => {
-    const promise = axios.get(msgURL);
-    promise.then(res => {
-            console.log(res);
-            messages = res.data;
-            renderMessages();
-        }
-    )
-    promise.catch(err => {
-            console.log(err);
-        }
-    )
-}
-, 3000);
